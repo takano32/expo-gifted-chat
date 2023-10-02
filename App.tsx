606cd52dc6bf77e4348c6d558bc17358eb3db4f7
TAKANO Mitsuhiro <takano32@gmail.com>
2023-10-02 23:48:50 +0900

    display chat view

--- App.tsx
+++ App.tsx
@@ -1,20 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useCallback, useEffect } from 'react'
+import { GiftedChat, IMessage } from 'react-native-gifted-chat'
 
-export default function App() {
+export default function Example() {
+  const [messages] = useState([])
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <GiftedChat
+      messages={messages}
+      user={{
+        _id: 1,
+      }}
+    />
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
